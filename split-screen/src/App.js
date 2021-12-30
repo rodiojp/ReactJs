099c3e5c84008d8dbbb4bd53ef1b82181c87ff5c
yurirod <yurirobins@gmail.com>
2021-12-30 10:33:12 -0500

    Lists and list items

--- split-screen/src/App.js
+++ split-screen/src/App.js
@@ -1,4 +1,41 @@
+import { LargePersonListItem } from './components/people/LargePersonListItem';
+import { SmallPersonListItem } from './components/people/SmallPersonListItem';
+import { RegularList } from './components/RegularList';
 import { SplitScreen } from './components/SplitScreen';
+
+const people = [{
+    name: 'John Doe',
+    age: 54,
+    hairColor: 'brown',
+    hobbies: ['swimming', 'bicycling', 'video games'],
+}, {
+    name: 'Brenda Smith',
+    age: 33,
+    hairColor: 'black',
+    hobbies: ['golf', 'mathematics'],
+}, {
+    name: 'Jane Garcia',
+    age: 27,
+    hairColor: 'blonde',
+    hobbies: ['biology', 'medicine', 'gymnastics'],
+}];
+
+const products = [{
+    name: 'Flat-Screen TV',
+    price: '$300',
+    description: 'Huge LCD screen, a great deal',
+    rating: 4.5,
+}, {
+    name: 'Basketball',
+    price: '$10',
+    description: 'Just like the pros use',
+    rating: 3.8,
+}, {
+    name: 'Running Shoes',
+    price: '$120',
+    description: 'State-of-the-art technology for optimum running',
+    rating: 4.2,
+}];
 
 const LeftHandComponent = ({ name }) => {
     return <h1 style={{ backgroundColor: 'green' }}>{name}</h1>
@@ -10,10 +47,20 @@ const RightHandComponent = ({ message }) => {
 
 function App() {
     return (
-        <SplitScreen leftWeight={1} rightWeight={3}>
-            <LeftHandComponent name='John'/>
-            <RightHandComponent message='Hello World!'/>
-        </SplitScreen>
+        <>
+            <SplitScreen leftWeight={1} rightWeight={3}>
+                <LeftHandComponent name='John' />
+                <RightHandComponent message='Hello World!' />
+            </SplitScreen>
+            <RegularList
+                items={people}
+                resourceName='person'
+                itemComponent={SmallPersonListItem} />
+            <RegularList
+                items={people}
+                resourceName='person'
+                itemComponent={LargePersonListItem} />
+    </>
     );
 }
 
