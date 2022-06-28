9f13c080bb3bdd567348c1898e1156cecca53798
yurirod <yurirobins@gmail.com>
2022-06-28 19:19:57 -0400

    React components in multiple places

--- simple/like_button.js
+++ simple/like_button.js
@@ -3,24 +3,31 @@
 const e = React.createElement;
 
 class LikeButton extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = { liked: false };
-  }
-
-  render() {
-    if (this.state.liked) {
-      return 'You liked this.';
+    constructor(props) {
+        super(props);
+        this.state = { liked: false };
     }
 
-    return e(
-      'button',
-      { onClick: () => this.setState({ liked: true }) },
-      'Like'
-    );
-  }
+    render() {
+        if (this.state.liked) {
+            return 'You liked comment number ' + this.props.commentID;
+        }
+
+        return e(
+            'button',
+            { onClick: () => this.setState({ liked: true }) },
+            'Like'
+        );
+    }
 }
 
-const domContainer = document.querySelector('#like_button_container');
-const root = ReactDOM.createRoot(domContainer);
-root.render(e(LikeButton));
+// Find all DOM containers, and render Like buttons into them.
+document.querySelectorAll('.like_button_container')
+    .forEach(domContainer => {
+        // Read the comment ID from a data-* attribute.
+        const commentID = parseInt(domContainer.dataset.commentid, 10);
+        const root = ReactDOM.createRoot(domContainer);
+        root.render(
+            e(LikeButton, { commentID: commentID })
+        );
+    });
